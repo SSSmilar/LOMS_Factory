974f72d9eac3a2f94c686bec2b2b75fa89738497
Andrey <realdeadinst@gmail.com>
2026-06-28 21:58:31 +0300

    feat(repo): refactor in-memory repository methods and add ListPartsByType

--- inventory/pkg/repository/memory.go
+++ inventory/pkg/repository/memory.go
@@ -2,11 +2,12 @@ package repository
 
 import (
 	"context"
+	"fmt"
+	"log/slog"
 	"sync"
 
 	"github.com/SSSmilar/LOMS_Factory/inventory/pkg/service"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+	inventoryv1 "github.com/SSSmilar/LOMS_Factory/shared/pkg/proto/inventory/v1"
 )
 
 type inMemoryRepo struct {
@@ -14,25 +15,27 @@ type inMemoryRepo struct {
 	mtx   sync.RWMutex
 }
 
-func (rep *inMemoryRepo) NewMemoryRepo() *inMemoryRepo {
+func NewMemoryRepo() *inMemoryRepo {
 	return &inMemoryRepo{
 		parts: make(map[string]service.Part),
 	}
 }
-func (rep *inMemoryRepo) GetParts(ctx context.Context, id string) (service.Part, error) {
+func (rep *inMemoryRepo) GetPart(ctx context.Context, id string) (service.Part, error) {
 	rep.mtx.RLock()
 	defer rep.mtx.RUnlock()
 	part, ok := rep.parts[id]
 	if !ok {
-		sb := status.New(codes.NotFound, "NOT_FOUND")
-		return part, sb.Err()
+		slog.Error("part not found")
+		return part, fmt.Errorf("part not found in method GetPart")
 	}
 	return part, nil
 }
-func (rep *inMemoryRepo) ListParts(ctx context.Context, ids []uuid.UUID) ([]service.Part, error) {
+func (rep *inMemoryRepo) GetParts(ctx context.Context, ids []string) ([]service.Part, error) {
 	parts := make([]service.Part, 0, len(ids))
+
 	rep.mtx.RLock()
 	defer rep.mtx.RUnlock()
+	
 	for _, id := range ids {
 		part, ok := rep.parts[id]
 		if !ok {
@@ -41,4 +44,15 @@ func (rep *inMemoryRepo) ListParts(ctx context.Context, ids []uuid.UUID) ([]serv
 		parts = append(parts, part)
 	}
 	return parts, nil
+}
+func (rep *inMemoryRepo) ListPartsByType(ctx context.Context, targetType inventoryv1.PartType) ([]service.Part, error) {
+	var result []service.Part
+	rep.mtx.RLock()
+	defer rep.mtx.RUnlock()
+	for _, partItem := range rep.parts {
+		if partItem.PartType == targetType {
+			result = append(result, partItem)
+		}
+	}
+	return result, nil
 }
